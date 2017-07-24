da28ca81fb1189c957b1c5e3408611f774340825
jugatsingh <jugatsingh.lamba2014@vit.ac.in>
2017-07-24 15:06:40 +0530

    turbulence demo

--- script.py
+++ script.py
@@ -3,35 +3,58 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img_rgb = cv2.imread('wing.png')
-img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-template1 = cv2.imread('shoelace1.png',0)
-template2 = cv2.imread('shoelace2.png',0)
-w1, h1 = template1.shape[::-1]
-w2, h2 = template2.shape[::-1]
+fourcc = cv2.VideoWriter_fourcc(*'X264')
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (1680,1050))
+cap = cv2.VideoCapture('/home/mbt/Dropbox/Airbus_Videos/Video 1_A350-0059-Flight-0009-150630-150830-LHWING2_1.mp4')
+font = cv2.FONT_HERSHEY_SIMPLEX
+i = 0
+while(True):
 
-res1 = cv2.matchTemplate(img_gray,template1,cv2.TM_CCOEFF_NORMED)
-res2 = cv2.matchTemplate(img_gray,template2,cv2.TM_CCOEFF_NORMED)
+	# Capture frame-by-frame
+	ret, frame = cap.read()
+	if ret == False:
+		break
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-print(type(res2))
-threshold = 0.95
+	# Display the resulting frame
+	# cv2.imshow('frame',frame)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+	template1 = cv2.imread('shoelace1.png',0)
+	template2 = cv2.imread('shoelace2.png',0)
+	w1, h1 = template1.shape[::-1]
+	w2, h2 = template2.shape[::-1]
 
-loc1 = np.where( res1 >= threshold)
+	res1 = cv2.matchTemplate(gray,template1,cv2.TM_CCOEFF_NORMED)
+	res2 = cv2.matchTemplate(gray,template2,cv2.TM_CCOEFF_NORMED)
 
-for pt in zip(*loc1[::-1]):
-    cv2.rectangle(img_rgb, pt, (pt[0] + w1, pt[1] + h1), (0,0,255), 2)
+	# print(type(res2))
+	threshold = 0.95
 
-threshold = 0.8
+	loc1 = np.where( res1 >= threshold)
 
-loc2 = np.where( res2 >= threshold)
+	for pt in zip(*loc1[::-1]):
+		cv2.rectangle(frame, pt, (pt[0] + w1, pt[1] + h1), (0,255,0), 2)
 
-loc2 = [pt for pt in loc2 if pt not in loc1]
+	threshold = 0.8
 
-for pt in zip(*loc2[::-1]):
-    cv2.rectangle(img_rgb, pt, (pt[0] + w2, pt[1] + h2), (0,0,255), 2)
+	loc2 = np.where( res2 >= threshold)
 
+	# loc2 = [pt for pt in loc2 if pt not in loc1]
 
-#cv2.imshow('res.png',img_rgb)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
-#cv2.imwrite('res.png',img_rgb)
+	for pt in zip(*loc2[::-1]):
+		cv2.rectangle(frame, pt, (pt[0] + w2, pt[1] + h2), (0,255,0), 2)
+	if len(loc2[0])<150:
+		
+		cv2.putText(frame,'EXTREME TURBULENCE!',(int(frame.shape[0]/2)-200,int(frame.shape[1]/2)-80), font, 4,(0,0,255),2,cv2.LINE_AA)
+	elif len(loc2[0])<220:	
+		cv2.putText(frame,'TURBULENCE!',(int(frame.shape[0]/2)-50,int(frame.shape[1]/2)-80), font, 4,(0,0,255),2,cv2.LINE_AA)
+	cv2.putText(frame,str(len(loc2[0])),(int(frame.shape[0])-150, 200), font, 4,(255,255,255),2,cv2.LINE_AA)
+	# print(len(loc1[0]),len(l))
+	# cv2.imshow('frame',frame)
+	out.write(frame)
+	# cv2.waitKey(0)
+	# cv2.destroyAllWindows()
+	#cv2.imwrite('res.png',frame)
+cap.release()
+out.release()
