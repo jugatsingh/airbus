476f0681793d63d84a9fcc56798d140a9f5a4424
jugatsingh <jugatsingh.lamba2014@vit.ac.in>
2017-07-28 15:47:57 +0530

    consistent recog. logic

--- script.py
+++ script.py
@@ -5,28 +5,38 @@ from matplotlib import pyplot as plt
 import math
 
 def l2_dist_check(pt, loc, dist):
-	for pt1 in zip(*loc[::-1]):
-		if math.sqrt(((pt1[0]-pt[0])**2) + ((pt1[1]-pt[1])**2)) < dist:
-			return False
-	return True
+    for pt1 in loc:
+        if math.sqrt(((pt1[0]-pt[0])**2) + ((pt1[1]-pt[1])**2)) < dist:
+            return (False, pt1)
+    return True,1
 
 fourcc = cv2.VideoWriter_fourcc(*'X264')
 out = cv2.VideoWriter('output.avi',fourcc, 20.0, (1680,1050))
 cap = cv2.VideoCapture('/home/mbt/Dropbox/Airbus_Videos/Video 1_A350-0059-Flight-0009-150630-150830-LHWING2_1.mp4')
 font = cv2.FONT_HERSHEY_SIMPLEX
+detected_markers = {}
+lookup = []
 # frame = cv2.imread('wing.png',1)
 # gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 i = 0
+n = -1.5
+step = 0.0
+#loop over the frames
 while(True):
-    ret, frame = cap.read()
-    if ret == False:
-    	break
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    i += 1
+    if i%75 == 0:
+        for pt in detected_markers:
+            lookup_table.append(pt)
+        detected_markers = {}
 
-    # Display the resulting frame
-    # cv2.imshow('frame',frame)
+    lookup_table = []
+    ret, frame = cap.read()
+    if ret == False: #logic for end of video
+        break
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     if cv2.waitKey(1) & 0xFF == ord('q'):
-    	break
+        break
     template1 = cv2.imread('shoelace1.png',0)
     template2 = cv2.imread('shoelace2.png',0)
     template3 = cv2.imread('shoelace3.png',0)
@@ -37,7 +47,7 @@ while(True):
     res2 = cv2.matchTemplate(gray,template2,cv2.TM_CCOEFF_NORMED)
     res3 = cv2.matchTemplate(gray,template3,cv2.TM_CCOEFF_NORMED)
 
-    # print(type(res2))
+    
     threshold1 = 0.97
     threshold3 = 0.9
     threshold2 = 0.8
@@ -45,25 +55,66 @@ while(True):
     loc1 = np.where( res1 >= threshold1)
     loc2 = np.where( res2 >= threshold2)
     loc3 = np.where( res3 >= threshold3)
+    loc1 = [pt for pt in zip(*loc1[::-1])]
+    loc2 = [pt for pt in zip(*loc2[::-1])]
+    loc3 = [pt for pt in zip(*loc3[::-1])]
 
-    for pt in zip(*loc1[::-1]):
-    	cv2.rectangle(frame, pt, (pt[0] + w1, pt[1] + h1), (0,255,0), 2)
     
-    loc3 = [pt for pt in zip(*loc3[::-1]) if l2_dist_check(pt, loc2, 50) == True]
-    loc3 = [pt for pt in loc3 if l2_dist_check(pt, loc1, 50) == True]
-    loc2 = [pt for pt in zip(*loc2[::-1]) if l2_dist_check(pt,loc1,15) == True]
+    temp = loc3[:]
+    for x in range(len(loc3)):
+        if l2_dist_check(loc3[x],loc3[x+1:], 10)[0] != True:
+            temp.remove(loc3[x])
+    loc3 = temp[:] 
+    # loc3 = [pt for pt in zip(*loc3[::-1]) if l2_dist_check(pt,loc3,3) == True]
+    loc3 = [pt for pt in loc3 if l2_dist_check(pt, loc2, 50)[0] == True]
+    loc3 = [pt for pt in loc3 if l2_dist_check(pt, loc1, 50)[0] == True]
+    loc2 = [pt for pt in loc2 if l2_dist_check(pt, loc1, 15)[0] == True]
 
+    #update detected_markers for every frame
+    for pt in loc1:
+        res = l2_dist_check(pt, list(detected_markers.keys()), 8)
+        if res[0] == True:
+            detected_markers[pt] = 1
+        else:
+            detected_markers[res[1]] += 1
     for pt in loc2:
-    	cv2.rectangle(frame, pt, (pt[0] + w2, pt[1] + h2), (0,255,0), 2)
-    
+        res = l2_dist_check(pt, list(detected_markers.keys()), 8)
+        if res[0] == True:
+            detected_markers[pt] = 1
+        else:
+            detected_markers[res[1]] += 1
     for pt in loc3:
-    	cv2.rectangle(frame, pt, (pt[0] + w3, pt[1] + h3), (0,255,0), 2)
+        res = l2_dist_check(pt, list(detected_markers.keys()), 8)
+        if res[0] == True:
+            detected_markers[pt] = 1
+        else:
+            detected_markers[res[1]] += 1
+    
+    #update lookup table for every frame based on std. deviation
+    # for pt in detected_markers:
+    #     # print(detected_markers[pt])
+    #     if detected_markers[pt] > (np.mean(list(detected_markers.values())) + n*np.std(list(detected_markers.values()))):
+    #         lookup_table.append(pt)
+
+    #snippet for drawing on every frame
+    for pt in lookup_table:
+        res1 = l2_dist_check(pt, loc1, 3)
+        res2 = l2_dist_check(pt, loc2, 3)
+        res3 = l2_dist_check(pt, loc3, 3)
+        if res1[0] == False:
+            cv2.rectangle(frame, res1[1], (res1[1][0] + w1, res1[1][1] + h1), (0,255,0), 2)    #print
+        elif res2[0] == False:
+            cv2.rectangle(frame, res2[1], (res2[1][0] + w2, res2[1][1] + h2), (0,255,0), 2)    #green
+        elif res3[0] ==False:    
+            cv2.rectangle(frame, res3[1], (res3[1][0] + w3, res3[1][1] + h3), (0,255,0), 2)    #markers
+        else:
+            cv2.rectangle(frame, pt, (pt[0] + w2, pt[1] + h2), (0,0,255), 2)    #print red markers
 
     if len(loc2)<150:
 
-    	cv2.putText(frame,'EXTREME TURBULENCE!',(int(frame.shape[0]/2)-200,int(frame.shape[1]/2)-80), font, 4,(0,0,255),2,cv2.LINE_AA)
+        cv2.putText(frame,'EXTREME TURBULENCE!',(int(frame.shape[0]/2)-200,int(frame.shape[1]/2)-80), font, 4,(0,0,255),2,cv2.LINE_AA)
     elif len(loc2)<220:
-    	cv2.putText(frame,'TURBULENCE!',(int(frame.shape[0]/2)-50,int(frame.shape[1]/2)-80), font, 4,(0,0,255),2,cv2.LINE_AA)
+        cv2.putText(frame,'TURBULENCE!',(int(frame.shape[0]/2)-50,int(frame.shape[1]/2)-80), font, 4,(0,0,255),2,cv2.LINE_AA)
     cv2.putText(frame,str(len(loc2[0])),(int(frame.shape[0])-150, 200), font, 4,(255,255,255),2,cv2.LINE_AA)
     # print(len(loc1[0]),len(l))
     # cv2.imshow('frame',frame)
